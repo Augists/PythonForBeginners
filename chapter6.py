47a21f99fabd29c571cba3d30ef564d7c7751855
Augists <awzyc2010@163.com>
2020-06-02 17:29:56 +0800

    chapter 6 finish

--- chapter6.py
+++ chapter6.py
@@ -202,6 +202,85 @@
 
 
 # ==================================================
+## inside @ aliens.py
+# alien_0 = {'color': 'green', 'points': 5}
+# alien_1 = {'color': 'yellow', 'points': 10}
+# alien_2 = {'color': 'red', 'points': 5}
+
+# aliens = [alien_0, alien_1, alien_2]
+# for alien in aliens:
+#     print(alien)
+
+
 # ==================================================
+## for range to creating
+# empty list
+# aliens = []
+
+# # 30 aliens
+# for alien_number in range(30):
+#     new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+#     aliens.append(new_alien)
+
+# # show the top 5 aliens
+# for alien in aliens[ : 5]:
+#     print(alien)
+# print("...")
+
+# # show how many aliens have been created
+# print("Total number of aliens: " +
+#         str(len(aliens)))
+
+
 # ==================================================
+## get list inside @ pizzas.py
+# store information
+# pizza = {
+#     'crust': 'thick',
+#     'toppings': ['mushrooms', 'extracheese']
+# }
+
+# print("You ordered a " + pizza['crust'] + "-crust pizza " +
+#         "with the following toppings:")
+
+# for topping in pizza['toppings']:
+#     print("\t" + topping)
+
+
 # ==================================================
+## inside and inside
+# favorite_languages = {
+#     'jen': ['python', 'ruby'],
+#     'sarah': ['c'],
+#     'edward': ['ruby', 'go'],
+#     'phil': ['python', 'haskell'],
+# }
+
+# for name, languages in favorite_languages.items():
+#     print("\n" + name.title() + "'s favorite languages are:")
+#     for language in languages:
+#         print("\t" + language.title())
+
+
+# ==================================================
+## dictionary inside dictionary @ many_user.py
+# users = {
+#     'aeinstein': {
+#         'first': 'albert',
+#         'last': 'einstein',
+#         'location': 'princeton',
+#     },
+#     'mcurie': {
+#         'first': 'marie',
+#         'last': 'curie',
+#         'location': 'paris',
+#     },
+# }
+
+# for username, user_info in users.items():
+#     print("\nUsername: " + username)
+#     full_name = user_info['first'] + " " + user_info['last']
+#     location = user_info['location']
+
+#     print("\tFull name: " + full_name.title())
+#     print("\tLocation: " + location.title())
